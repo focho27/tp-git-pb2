6827c6241d0fdbb6979c26f078a7d1e2aefb5c1c
focho27 <gerorod1999@gmail.com>
2020-09-21 16:27:43 -0300

    primer commit para tam

---
+++ src/tps/domain/Calculadora.java
@@ -0,0 +1,39 @@
+package tps.domain;
+import java.util.Scanner;
+public class Calculadora {
+	
+	
+	 public static void main(String[] args) {
+	 Integer a;
+	 Integer b;
+	 Integer opcion;
+	 Integer resultado;
+	 Scanner teclado = new Scanner(System.in);
+	do{
+	 System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+	 opcion = teclado.nextInt();
+	 }while(opcion < 1 && opcion > 4);
+	 System.out.println("Ingrese el primer numero");
+	 a = teclado.nextInt();
+	 System.out.println("Ingrese el segundo numero");
+	 b = teclado.nextInt();
+	 switch(opcion) {
+	 case 1:
+		 resultado = a + b;
+		 break;
+	 case 2:
+		 resultado = a - b;
+		 break;
+	 case 3:
+		 resultado = a * b;
+		 break;
+	 default:
+		 resultado = a / b;
+		 break;
+		 
+	 }
+	 
+	 System.out.println("El resultado es " + resultado.toString());
+	 }
+	
+}
